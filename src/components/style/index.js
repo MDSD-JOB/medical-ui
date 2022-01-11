6915999a0b6f7a6e88ec6fe84ea18e2f3aeb0655
LiJin <jin.l@medicalsystem.cn>
2022-01-11 17:10:51 +0800

    fix: 颜色模式

--- src/components/style/index.js
+++ src/components/style/index.js
@@ -1,11 +1,16 @@
-// import '../MedButton/src/index.less'
-// import '../MedSTable/src/index.less'
-// import '../MedSRadio/src/index.less'
-// import '../MedSCheckbox/src/index.less'
-// import '../MedSForm/src/index.less'
-// import '../MedSCard/src/index.less'
-// import '../MedSSticker/src/index.less'
+// 主题样式
 import './index.less'
+
+// 模式样式
 import './theme-antd-weak.css'
 import './theme-antd-gray.css'
 import './theme-antd-dark.css'
+
+// 组件样式
+import '../MedButton/src/index.less'
+import '../MedSTable/src/index.less'
+import '../MedSRadio/src/index.less'
+import '../MedSCheckbox/src/index.less'
+import '../MedSForm/src/index.less'
+import '../MedSCard/src/index.less'
+import '../MedSSticker/src/index.less'
